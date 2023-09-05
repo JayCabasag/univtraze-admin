e0f6a4fb693f206d5a5fc3962838c797a441f3a6
Jay Cabasag <jaycabasag1999@gmail.com>
2023-09-05 19:35:12 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <main className="underline">Hellow world</main>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./routes/auth/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "dashboard",
+    element: <div>About</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
