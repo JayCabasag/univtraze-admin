d3e3eec64d634a33985bb353d44ea6e1a4904922
Jay Cabasag <jaycabasag1999@gmail.com>
2023-09-26 20:11:08 +0800

    feat: added spacing

--- src/pages/(dashboard)/CommunicableDiseaseReportsPage.tsx
+++ src/pages/(dashboard)/CommunicableDiseaseReportsPage.tsx
@@ -4,10 +4,10 @@ import Header from '@/components/header/Header';
 
 export default function CommunicableDiseaseReportsPage() {
   return (
-    <main>
+    <main className='p-[15px]'>
       <Header title="Dashboard" sub={'Communicable disease reports'} />
       <h2 className="mt-[40px] text-[28px] font-bold text-[#364D39]">Communicable diease reports</h2>
-      <div className="p-[15px] mt-[40px] flex flex-col md:flex-row gap-[33px]">
+      <div className="mt-[40px] flex flex-col md:flex-row gap-[33px]">
         <CommunicableDiseaseReportTable />
         <CommunicableDiseaseReportDetails />
       </div>
